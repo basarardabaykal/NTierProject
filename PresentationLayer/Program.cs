6729b65fc2662b054ad301b7c09f81ac3cc65e36
ardanimo <ardanimoxd@gmail.com>
2025-06-30 15:18:13 +0300

    validation services

--- PresentationLayer/Program.cs
+++ PresentationLayer/Program.cs
@@ -4,6 +4,9 @@ using BusinessLayer.Repository;
 using BusinessLayer.Services.DbServices;
 using DataLayer;
 using Microsoft.EntityFrameworkCore;
+using FluentValidation.AspNetCore;
+using System.Reflection;
+using FluentValidation;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,14 +14,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers().AddRazorRuntimeCompilation();
 
+//dependency injection
 builder.Services.AddDbContext<UserDBContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserDbService, UserDbService>();
 
+//fluent validation
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddFluentValidationClientsideAdapters();
+builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
 
 //builder.Services.AddOpenApi();
 
+
+//api test
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
