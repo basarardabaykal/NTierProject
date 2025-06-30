73f5df214874cd552fb07de66e39447df041c948
ardanimo <ardanimoxd@gmail.com>
2025-06-30 14:04:04 +0300

    context factory added

---
+++ DataLayer/UserDBContextFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Design;
+using System.IO;
+
+namespace DataLayer
+{
+    internal class UserDBContextFactory
+    {
+        public UserDBContext CreateDbContext(string[] args)
+        {
+            // Load configuration from appsettings.json
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()) // Points to your project root
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<UserDBContext>();
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new UserDBContext(optionsBuilder.Options);
+        }
+    }
+}
