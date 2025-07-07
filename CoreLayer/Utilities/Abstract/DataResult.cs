2117db268ff607307953267765b613e68e6761c2
ardanimo <ardanimoxd@gmail.com>
2025-07-07 10:01:57 +0300

    dataresult parent class and interface created

---
+++ CoreLayer/Utilities/Abstract/DataResult.cs
@@ -0,0 +1,27 @@
+﻿using CoreLayer.Utilities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLayer.Utilities.Abstract
+{
+    public abstract class DataResult<T> : IDataResult<T>
+    {
+        public T Data { get; }
+        public bool Success { get; }
+        public string Message { get; }
+        public DataResult(T data, bool success, string message)
+        {
+            Data = data;
+            Success = success;
+            Message = message;
+        }
+        public DataResult(T data, bool success)
+        {
+            Data = data;
+            Success = success;
+        }
+    }
+}
---
+++ CoreLayer/Utilities/Interfaces/IDataResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLayer.Utilities.Interfaces
+{
+    public interface IDataResult<T>
+    {
+        public bool Success { get; }
+        public string Message { get; }
+        public T Data {  get; }
+
+
+    }
+}
