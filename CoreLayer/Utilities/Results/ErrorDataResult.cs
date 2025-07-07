3b35b19978bf7460145d64bfc0362d9cf6437136
ardanimo <ardanimoxd@gmail.com>
2025-07-07 10:13:06 +0300

    dataresults implemented

--- CoreLayer/Utilities/Results/ErrorDataResult.cs
+++ CoreLayer/Utilities/Results/ErrorDataResult.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoreLayer.Utilities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,30 @@ using System.Threading.Tasks;
 
 namespace CoreLayer.Utilities.Results
 {
-    public class ErrorDataResult
+    public class ErrorDataResult<T> : DataResult<T>
     {
+        public ErrorDataResult(T data, bool success, string message) : base(data, false, message)
+        {
+
+        }
+        public ErrorDataResult(T data, string message) : base(data, false, message)
+        {
+
+        }
+
+        public ErrorDataResult(T data) : base(data, false)
+        {
+
+        }
+
+        public ErrorDataResult(string message) : base(default, false, message)
+        {
+
+        }
+
+        public ErrorDataResult() : base(default, false)
+        {
+
+        }
     }
 }
--- CoreLayer/Utilities/Results/SuccessDataResult.cs
+++ CoreLayer/Utilities/Results/SuccessDataResult.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoreLayer.Utilities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,30 @@ using System.Threading.Tasks;
 
 namespace CoreLayer.Utilities.Results
 {
-    public class SuccessDataResult
+    public class SuccessDataResult<T> : DataResult<T>
     {
+        public SuccessDataResult(T data, bool success, string message) : base(data, true, message)
+        {
+
+        }
+        public SuccessDataResult(T data, string message) : base(data, true, message)
+        {
+
+        }
+
+        public SuccessDataResult(T data) : base(data, true)
+        {
+
+        }
+
+        public SuccessDataResult(string message) : base(default, true, message)
+        {
+
+        }
+
+        public SuccessDataResult() : base(default, true)
+        {
+
+        }
     }
 }
