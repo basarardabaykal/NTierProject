078471049f0e2650ee1eace4079ac5494fe00712
ardanimo <ardanimoxd@gmail.com>
2025-07-09 14:11:34 +0300

    id is now automatically generated

--- BusinessLayer/Dto/UserDTO.cs
+++ BusinessLayer/Dto/UserDTO.cs
@@ -8,7 +8,6 @@ namespace BusinessLayer.Dto;
 
 public class UserDTO
 {
-    public string Id { get; set; }
     public string Firstname { get; set; }
     public string Lastname { get; set; }
     public string Tcnumber { get; set; }
