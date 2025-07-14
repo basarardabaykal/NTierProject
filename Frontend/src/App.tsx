9945bcf38bd71cf527e97a992bb4df44d7d3ef96
ardanimo <ardanimoxd@gmail.com>
2025-07-14 11:03:47 +0300

    login frontend

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css'
 import HomePage from './assets/Pages/HomePage'
 import GetUser from './assets/Pages/GetUser'
+import Login from './assets/Pages/Login'
+import Register from './assets/Pages/Register'
 
 function App() {
 
   return (
     <>
       <div className='w-3/4 m-auto'>
-        <GetUser />
+        <Login />
+        <Register />
       </div>
     </>
   )
--- Frontend/src/assets/Pages/Login.tsx
+++ Frontend/src/assets/Pages/Login.tsx
@@ -1,5 +1,52 @@
+import { useState } from "react";
+import axios from "axios"
+
 export default function Login() {
-    return (
-        <></>
-    )
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post(" https://localhost:7297/api/auth/login", {
+        email: email,
+        password: password,
+      })
+      console.log(response.data)
+      if (response.data.success) {
+        localStorage.setItem("token", response.data.data.token)
+        console.log("Successfully logged in.")
+      }
+    } catch (error) {
+      console.log("An error occured.")
+    }
+  }
+  return (
+    <>
+      <div>
+        <div>
+          <div>
+            <p>Email</p>
+            <input
+              className="bg-white text-black  mb-4"
+              type="email"
+              value={email}
+              onChange={(e) => (setEmail(e.target.value))}
+            />
+          </div>
+
+          <div>
+            <p>Password</p>
+            <input
+              className="bg-white text-black mb-4"
+              type="password"
+              value={password}
+              onChange={(e) => (setPassword(e.target.value))}
+            />
+          </div>
+
+        </div>
+        <button onClick={handleSubmit}>Sign Up</button>
+      </div>
+    </>
+  )
 }
