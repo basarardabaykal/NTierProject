3be73d231d0e968c0e0f59c8cde8599e460b71b2
ardanimo <ardanimoxd@gmail.com>
2025-07-15 12:28:28 +0300

    interface signatures fixed

--- Backend/BusinessLayer/Congrate/Repository/IAuthRepository.cs
+++ Backend/BusinessLayer/Congrate/Repository/IAuthRepository.cs
@@ -14,5 +14,7 @@ namespace BusinessLayer.Congrate.Repository
         public Task<IDataResult<AppUser>> GetUserByEmail(string email);
         public Task<IDataResult<bool>> CheckPassword(AppUser user, string password);
         public Task<IDataResult<AppUser>> CreateUser(AppUser user, string password);
+        public Task<IDataResult<List<string>>> GetUserRoles(string email);
+        public  Task<IDataResult<bool>> AssignRole(AppUser user, string role);
     }
 }
--- Backend/BusinessLayer/Congrate/Services/DbServices/IAuthDbService.cs
+++ Backend/BusinessLayer/Congrate/Services/DbServices/IAuthDbService.cs
@@ -12,6 +12,7 @@ namespace BusinessLayer.Congrate.Services.DbServices
     public interface IAuthDbService
     {
         public Task<IDataResult<AppUser>> Login(string email, string password);
-        public Task<IDataResult<AppUser>> Register(string email, string password, string firstName, string lastName, string tcNumber, string userName);
+        public Task<IDataResult<AppUser>> Register(string email, string password, string firstName, string lastName, string tcNumber, string userName, string role);
+        public Task<IDataResult<List<string>>> GetUserRoles(string email);
     }
 }
