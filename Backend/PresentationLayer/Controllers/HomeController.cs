3f8aac5c610f62cab202914869f8c05378d4d3de
ardanimo <ardanimoxd@gmail.com>
2025-07-18 16:13:06 +0300

    only admin role can change company of a user

--- Backend/PresentationLayer/Controllers/HomeController.cs
+++ Backend/PresentationLayer/Controllers/HomeController.cs
@@ -66,7 +66,7 @@ namespace NTierProject.Controllers
             };
         }
 
-        [Authorize]
+        [Authorize(Roles ="Admin")]
         [HttpPatch("updatecompany")]
         public async Task<IActionResult> UpdateCompanyId([FromBody] UserDTO userDTO)
         {
