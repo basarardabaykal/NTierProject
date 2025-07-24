e609a42a0b11513d852e283a541ed324e306f237
ardanimo <ardanimoxd@gmail.com>
2025-07-24 11:34:39 +0300

    redundant claim decode removed

--- Frontend/src/context/AuthContext.tsx
+++ Frontend/src/context/AuthContext.tsx
@@ -4,10 +4,6 @@ import { jwtDecode } from "jwt-decode"
 import axios from "axios";
 
 interface DecodedToken {
-  "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier": string
-  "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name": string
-  "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress": string
-  "http://schemas.microsoft.com/ws/2008/06/identity/claims/role": string | string[]
   exp: number
 }
 
